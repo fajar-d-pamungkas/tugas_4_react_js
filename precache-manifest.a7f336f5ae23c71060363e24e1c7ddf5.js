5d6938eb190bf3191b19f0a146628cc16d1c9ee1
fajar-d-pamungkas <71364634+fajar-d-pamungkas@users.noreply.github.com>
2020-09-29 21:13:13 +0700

    Add files via upload

---
+++ precache-manifest.a7f336f5ae23c71060363e24e1c7ddf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "315f8a6ef3c6724980f06cd047c6ede0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6743b48f639a61a8ede4",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fe0f858930b2ba70b959",
+    "url": "/static/js/2.c20edc23.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c20edc23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6743b48f639a61a8ede4",
+    "url": "/static/js/main.9fc94387.chunk.js"
+  },
+  {
+    "revision": "d7a6e5881c5d479b7594",
+    "url": "/static/js/runtime-main.2ea4cdbd.js"
+  }
+]);
